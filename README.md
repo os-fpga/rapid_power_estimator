9249ba90436128c01fc9db145529ff1d333c25b7
Chern Yee Kok <chernyee.kok@rapidsilicon.com>
2024-02-08 15:51:48 +0800

    setup the electron frontend skeleton and update README

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Rapid Power Estimator
-<TODO>
 
-## Installation
-<TODO>
+Rapid Power Estimation Tool allows you to estimate power consumption at various stages of your design cycle. It simplifies the input of design information through intuitive wizards and delivers comprehensive power and thermal data analysis.
+
+## Pre-requisite
+
+The development workflow requires NodeJS at least version 21 or above to run. Please goto the official NodeJS website https://nodejs.org/ to download and install for your respective OS.
+
+At the command prompt or terminal, execute the commands below to check if NodeJS and npm are installed properly with the correct version:-
+
+- `$ node -v`
+- `$ npm -v`
+
+## Installation Steps
+
+1. Download the source code `git clone https://github.com/os-fpga/rapid_power_estimator`
+2. Change to source folder `cd rapid_power_estimator`
+3. Install Electron app dependencies `npm install`
+4. Install Python dependencies `pip install -r requirements.txt`
+
+### Launch the app (development stage)
+
+1 Launch the Electron desktop app by command `npm start`
---
+++ main.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow } = require("electron");
+const { spawn } = require("child_process");
+const path = require("node:path");
+
+const startFlaskServer = () => {
+  const device_xml = path.join(__dirname, "backend/etc/device.xml");
+  const apiServer = spawn(`python`, ["backend/restapi_server.py", device_xml]);
+
+  apiServer.stdout.on("data", (data) => {
+    console.log(`stdout:\n${data}`);
+  });
+
+  apiServer.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  apiServer.on("error", (error) => {
+    console.log(`error: ${error.message}`);
+  });
+
+  apiServer.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  apiServer.on("message", (message) => {
+    console.log(`message:\n${message}`);
+  });
+
+  return apiServer;
+};
+
+const createWindow = () => {
+  const win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile("src/index.html");
+};
+
+let child = null;
+
+app.whenReady().then(() => {
+  child = startFlaskServer();
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  child.kill("SIGINT");
+  if (process.platform !== "darwin") app.quit();
+});

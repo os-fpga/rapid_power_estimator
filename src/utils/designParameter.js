660ab4e846ba156c1e2620142bed13685d634b42
volodymyrkochyn <vovakochun@gmail.com>
2024-04-22 17:15:27 +0300

    Add Design Parameters values (no backend integration)

---
+++ src/utils/designParameter.js
@@ -0,0 +1,23 @@
+export const ddr = [
+  { id: 0, text: 'DDR3' },
+  { id: 1, text: 'DDR4' },
+];
+
+export const ddrRate = [
+  { id: 0, text: '1600' },
+  { id: 1, text: '1866' },
+  { id: 2, text: '2133' },
+];
+
+export const ddrWidth = [
+  { id: 0, text: '16-bit' },
+  { id: 1, text: '32-bit' },
+];
+
+export const endpoints = [
+  { id: 0, text: 'Fabric' },
+  { id: 1, text: 'ACPU' },
+  { id: 2, text: 'BCPU' },
+  { id: 3, text: 'SPI/QSPI' },
+  { id: 4, text: 'I2C' },
+];

1c7696adb5f883f90f5def266c0787644592088c
volodymyrkochyn <vovakochun@gmail.com>
2024-07-25 21:07:03 +0300

    Update peripherals component. Show only available peripherals

--- src/components/PeripheralsComponent.js
+++ src/components/PeripheralsComponent.js
@@ -59,32 +59,121 @@ function PeripheralRow({ data, size = 3 }) {
 
 function PeripheralsComponent({ device }) {
   const [dev, setDev] = React.useState(null);
-  const [i2c, setI2c] = React.useState({ name: 'I2C', power: 0 });
-  const [spi, setSpi] = React.useState({ name: 'SPI', power: 0 });
-  const [pwm, setPWM] = React.useState({ name: 'PWM', power: 0 });
-  const [usb2, setUsb2] = React.useState({ name: 'USB 2.0', power: 0 });
-  const [jtag, setJtag] = React.useState({ name: 'JTAG', power: 0 });
-  const [gige, setGige] = React.useState({ name: 'GigE', power: 0 });
-  const [uart0, setUart0] = React.useState({ name: 'UART0', power: 0 });
-  const [uart1, setUart1] = React.useState({ name: 'UART1', power: 0 });
   const [gpio, setGPIO] = React.useState({ name: 'GPIO', power: 0 });
   const { selectedItem } = useSelection();
   const { totalConsumption } = useSocTotalPower();
   const { socState } = useGlobalState();
+  const names = ['UART0', 'UART1', 'USB 2.0',
+    'I2C', 'GPIO', 'PWM', 'SPI', 'JTAG', 'GigE'];
+  const template = (index) => ({
+    name: names[index], messages: [], power: 0, percents: 100, valid: false,
+  });
+  const length = 9;
+  const dataDefault = Array.from({ length }, (_, index) => template(index));
+  const [tableData, setTableData] = React.useState(dataDefault);
 
   function fetchPeripherals(deviceId, key, url, name) {
     server.GET(server.peripheralPath(deviceId, url), (data) => {
-      if (key === 'i2c') setI2c({ name, power: data.consumption.block_power });
-      if (key === 'spi') setSpi({ name, power: data.consumption.block_power });
-      if (key === 'pwm') setPWM({ name, power: data.consumption.block_power });
-      if (key === 'usb2') setUsb2({ name, power: data.consumption.block_power });
-      if (key === 'jtag') setJtag({ name, power: data.consumption.block_power });
-      if (key === 'gige') setGige({ name, power: data.consumption.block_power });
-      if (key === 'uart') {
-        if (data.index === 0) setUart0({ name, power: data.consumption.block_power });
-        else setUart1({ name, power: data.consumption.block_power });
+      if (key === 'i2c') {
+        const newData = [...tableData];
+        newData[3] = {
+          name,
+          power: data.consumption.block_power,
+          messages: socState.i2c,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
       }
-      if (key === 'gpio') setGPIO((prev) => ({ ...prev, power: data.consumption.block_power + 1 }));
+      if (key === 'spi') {
+        const newData = [...tableData];
+        newData[6] = {
+          name,
+          power: data.consumption.block_power,
+          messages: socState.spi,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
+      }
+      if (key === 'pwm') {
+        const newData = [...tableData];
+        newData[5] = {
+          name,
+          power: data.consumption.block_power,
+          messages: socState.pwm,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
+      }
+      if (key === 'usb2') {
+        const newData = [...tableData];
+        newData[2] = {
+          name,
+          power: data.consumption.block_power,
+          messages: socState.usb2,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
+      }
+      if (key === 'jtag') {
+        const newData = [...tableData];
+        newData[7] = {
+          name,
+          power: data.consumption.block_power,
+          messages: socState.jtag,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
+      }
+      if (key === 'gige') {
+        const newData = [...tableData];
+        newData[8] = {
+          name,
+          power: data.consumption.block_power,
+          messages: socState.gige,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
+      }
+      if (key === 'uart') {
+        if (data.index === 0) {
+          const newData = [...tableData];
+          newData[0] = {
+            name,
+            power: data.consumption.block_power,
+            messages: socState.uart0,
+            percents: 100,
+            valid: true,
+          };
+          setTableData(newData);
+        } else {
+          const newData = [...tableData];
+          newData[1] = {
+            name,
+            power: data.consumption.block_power,
+            messages: socState.uart1,
+            percents: 100,
+            valid: true,
+          };
+          setTableData(newData);
+        }
+      }
+      if (key === 'gpio') {
+        const newData = [...tableData];
+        newData[4] = {
+          name,
+          power: data.consumption.block_power + newData[4].power,
+          messages: socState.gpio,
+          percents: 100,
+          valid: true,
+        };
+        setTableData(newData);
+      }
     });
   }
 
@@ -92,11 +181,14 @@ function PeripheralsComponent({ device }) {
     setDev(device);
     if (device !== null) {
       setGPIO({ ...gpio, power: 0 });
+      setTableData(dataDefault);
       server.GET(server.api.fetch(server.Elem.peripherals, device), (data) => {
         data.forEach((entry) => {
           fetchPeripherals(device, entry.type, entry.href, entry.name);
         });
       });
+    } else {
+      setTableData(dataDefault);
     }
   }
 
@@ -107,6 +199,19 @@ function PeripheralsComponent({ device }) {
   }
 
   const peripherals = totalConsumption.processing_complex.dynamic.components.find((elem) => elem.type === 'peripherals');
+
+  function getSlicedData(row) {
+    let initValue = row * 3;
+    const res = [];
+    tableData.forEach((item) => {
+      if (item.valid) {
+        if (initValue !== 0) {
+          initValue -= 1;
+        } else if (res.length < 3) res.push(item);
+      }
+    });
+    return res;
+  }
 
   return (
     <div className={getClassName()}>
@@ -121,42 +226,9 @@ function PeripheralsComponent({ device }) {
           {' %'}
         </div>
       </div>
-      <PeripheralRow
-        data={[{
-          name: uart0.name, messages: socState.uart0, power: uart0.power, percents: 100,
-        },
-        {
-          name: uart1.name, messages: socState.uart1, power: uart1.power, percents: 100,
-        },
-        {
-          name: usb2.name, messages: socState.usb2, power: usb2.power, percents: 100,
-        },
-        ]}
-      />
-      <PeripheralRow
-        data={[{
-          name: i2c.name, messages: socState.i2c, power: i2c.power, percents: 100,
-        },
-        {
-          name: gpio.name, messages: socState.gpio, power: gpio.power, percents: 100,
-        },
-        {
-          name: pwm.name, messages: socState.pwm, power: pwm.power, percents: 100,
-        },
-        ]}
-      />
-      <PeripheralRow
-        data={[{
-          name: spi.name, messages: socState.spi, power: spi.power, percents: 100,
-        },
-        {
-          name: jtag.name, messages: socState.jtag, power: jtag.power, percents: 100,
-        },
-        {
-          name: gige.name, messages: socState.gige, power: gige.power, percents: 100,
-        },
-        ]}
-      />
+      <PeripheralRow data={getSlicedData(0)} />
+      <PeripheralRow data={getSlicedData(1)} />
+      <PeripheralRow data={getSlicedData(2)} />
     </div>
   );
 }

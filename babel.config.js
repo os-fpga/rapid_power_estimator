610d93cab0429be6380fd40eb7dd85197b65b0a4
Shiva Ahir <shivaahir158@gmail.com>
2024-09-05 11:22:00 -0700

    Add Front-End Code Coverage with Jest and Backend PyTests for BRAM API Endpoints (#153)
    * adding the lines to upload the npm coverage report
    
    * added the necessary packages to run the npm test
    
    * Deleted App.test.js as it's no longer needed
    
    * updating rpe_test.yml to include front-end test coverage using npm and Codecov
    
    * doing changes for codecov > pytests and npm tests
    
    * PyTests for BRAM API endpoints
    
    * adding necessary packages and changes in config files
    
    * doing changres in jest.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  env: {
+    test: {
+      plugins: ['istanbul']
+    }
+  }
 };
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
   testEnvironment: 'jsdom',
+  
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",  
+    "!src/**/*.test.{js,jsx}", 
+    "!src/**/index.{js,jsx}", 
+  ],
+  coverageReporters: ['text', 'lcov'],  
+  coverageDirectory: "coverage",  
 };

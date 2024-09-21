615d06a0e68d132f56f9ec38a606f9ba0392dac4
Shiva Ahir <shivaahir158@gmail.com>
2024-09-21 02:09:06 -0400

    fixing the RPE crash error when editing gpio peripheral in popup dialog

--- src/components/ModalWindows/PeripheralsModal.js
+++ src/components/ModalWindows/PeripheralsModal.js
@@ -1,39 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FieldType, getPerformance } from '../../utils/common';
 import ModalWindow from './ModalWindow';
+
+function useDisableEnterKey() {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+}
+
+function getFields(defaultValue, index) {
+  return [
+    {
+      fieldType: FieldType.select,
+      id: 'usage',
+      text: 'Usage',
+      value: defaultValue.data[index].data.usage,
+      values: defaultValue.usage,
+    },
+    {
+      fieldType: FieldType.select,
+      id: defaultValue.performance_id,
+      text: 'Performance',
+      value: getPerformance(defaultValue.data[index].data),
+      values: defaultValue.performance,
+    },
+    {
+      disable: !defaultValue.io_used,
+      fieldType: FieldType.number,
+      id: 'io_used',
+      text: 'IO Used',
+      value: defaultValue.data.io_used,
+    },
+  ];
+}
 
 function PeripheralsModal({
   closeModal, onSubmit, defaultValue, index,
 }) {
+  useDisableEnterKey();
+
   return (
     <ModalWindow
       closeModal={closeModal}
       onSubmit={onSubmit}
       defaultValue={defaultValue.data[index].data}
       title={defaultValue.data[index].data.name}
-      fields={[
-        {
-          fieldType: FieldType.select,
-          id: 'usage',
-          text: 'Usage',
-          value: defaultValue.data[index].data.usage,
-          values: defaultValue.usage,
-        },
-        {
-          fieldType: FieldType.select,
-          id: defaultValue.performance_id,
-          text: 'Performance',
-          value: getPerformance(defaultValue.data[index].data),
-          values: defaultValue.performance,
-        },
-        {
-          disable: !defaultValue.io_used,
-          fieldType: FieldType.number,
-          id: 'io_used',
-          text: 'IO Used',
-          value: defaultValue.data.io_used,
-        },
-      ]}
+      fields={getFields(defaultValue, index)}
     />
   );
 }

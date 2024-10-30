28de7a55b1d97be94e97a94a258f998108ab91e3
Shiva Ahir <shivaahir158@gmail.com>
2024-10-30 00:33:29 -0400

    Setting up Playwright testing env and config.js, adding e2e folder under tests (#250)
    * adding e2e folder under tests
    
    * adding condition for e2e tests in jest.config.js
    
    * Setting up e2e under test GitHub ci setup (#253)
    
    ---------
    
    Co-authored-by: Ravikiran Chollangi <ravikiran.chollangi@gmail.com>
    Co-authored-by: Nadeem Yaseen <70559777+NadeemYaseen@users.noreply.github.com>

---
+++ tests/e2e/fle.test.js
@@ -0,0 +1,25 @@
+const { _electron: electron } = require('playwright');
+const { test, expect } = require('@playwright/test');
+
+test('Launch Electron app and click on FLE block', async () => {
+  const app = await electron.launch({ args: ['main.js'] });
+
+  const window = await app.firstWindow();
+
+    // selecting the device (MPW1 Gemini)
+  const deviceDropdown = await window.waitForSelector('#deviceId');
+  await deviceDropdown.selectOption('MPW1');
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const fleBlock = await window.waitForSelector('#app > div > div.top-row-container > div.main-table-container.main-border > div.top-l2 > div.top-l2-col2 > div.top-l2-col2-elem > div > div:nth-child(2) > div:nth-child(2) > div');
+  await fleBlock.click();
+
+  const flePowerVisible = await window.isVisible('div.title-comp-total-text');
+  expect(flePowerVisible).toBeTruthy();
+
+  console.log('FLE block clicked and verified.');
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  await app.close();
+});

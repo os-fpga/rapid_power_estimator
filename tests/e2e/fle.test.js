06b39d658f1fa6c68c1fcd14072ebafa812f3e3d
Shiva Ahir <shivaahir158@gmail.com>
2024-11-08 13:12:12 -0800

    Update fle.test.js

--- tests/e2e/fle.test.js
+++ tests/e2e/fle.test.js
@@ -6,11 +6,12 @@
 
    const window = await app.firstWindow();
 
-     // selecting the device (MPW1 Gemini)
+   // selecting the device (MPW1 Gemini)
    const deviceDropdown = await window.waitForSelector('#deviceId');
    await deviceDropdown.selectOption('MPW1');
    await new Promise((resolve) => setTimeout(resolve, 2000));
 
+   // Clicking on the FLE block
    const fleBlock = await window.waitForSelector('#app > div > div.top-row-container > div.main-table-container.main-border > div.top-l2 > div.top-l2-col2 > div.top-l2-col2-elem > div > div:nth-child(2) > div:nth-child(2) > div');
    await fleBlock.click();
 

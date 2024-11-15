446c1e38d45a1a92aef20bd08ec2454773e29e30
Shiva Ahir <shivaahir158@gmail.com>
2024-11-15 09:08:11 -0800

    Update acpu.test.js

--- tests/e2e/acpu.test.js
+++ tests/e2e/acpu.test.js
@@ -1,31 +1,31 @@
-const { _electron: electron } = require('playwright');
-const { test, expect } = require('@playwright/test');
+// const { _electron: electron } = require('playwright');
+// const { test, expect } = require('@playwright/test');
 
-test('Launch Electron app, select device, toggle ACPU power, and click Add', async () => {
-  const app = await electron.launch({ args: ['main.js'] });
-  const window = await app.firstWindow();
+// test('Launch Electron app, select device, toggle ACPU power, and click Add', async () => {
+//   const app = await electron.launch({ args: ['main.js'] });
+//   const window = await app.firstWindow();
 
-  // Selecting the device (MPW1 Gemini)
-  const deviceDropdown = await window.waitForSelector('#deviceId');
-  await deviceDropdown.selectOption('MPW1');
+//   // Selecting the device (MPW1 Gemini)
+//   const deviceDropdown = await window.waitForSelector('#deviceId');
+//   await deviceDropdown.selectOption('MPW1');
 
-  // Clicking on ACPU block
-  const acpuBlockSelector = '#app > div > div.top-row-container > div.main-table-container.main-border > div.top-l2 > div.top-l2-col1 > div.top-l2-col1-row1 > div:nth-child(1) > div';
-  await window.click(acpuBlockSelector);
+//   // Clicking on ACPU block
+//   const acpuBlockSelector = '#app > div > div.top-row-container > div.main-table-container.main-border > div.top-l2 > div.top-l2-col1 > div.top-l2-col1-row1 > div:nth-child(1) > div';
+//   await window.click(acpuBlockSelector);
 
-  // Toggling ACPU power
-  const acpuPowerToggleSelector = '#app > div > div.table-container.main-border > div > div.toggle-container > label.toggle-switch > span';
-  await window.click(acpuPowerToggleSelector);
+//   // Toggling ACPU power
+//   const acpuPowerToggleSelector = '#app > div > div.table-container.main-border > div > div.toggle-container > label.toggle-switch > span';
+//   await window.click(acpuPowerToggleSelector);
 
-  // Click on Add button
-  const addButtonSelector = '#app > div > div.table-container.main-border > div > div.cpu-container > div.table-wrapper > button';
-  await window.click(addButtonSelector);
+//   // Click on Add button
+//   const addButtonSelector = '#app > div > div.table-container.main-border > div > div.cpu-container > div.table-wrapper > button';
+//   await window.click(addButtonSelector);
 
-  // Click OK in the popup
-  const okButtonSelector = 'body > div:nth-child(3) > div > div.ant-modal-wrap > div > div:nth-child(1) > div > div.ant-modal-footer > button.ant-btn.css-dev-only-do-not-override-vryruh.ant-btn-primary.ant-btn-color-primary.ant-btn-variant-solid > span';
-  await window.click(okButtonSelector);
+//   // Click OK in the popup
+//   const okButtonSelector = 'body > div:nth-child(3) > div > div.ant-modal-wrap > div > div:nth-child(1) > div > div.ant-modal-footer > button.ant-btn.css-dev-only-do-not-override-vryruh.ant-btn-primary.ant-btn-color-primary.ant-btn-variant-solid > span';
+//   await window.click(okButtonSelector);
 
-  console.log('ACPU power and verified.');
+//   console.log('ACPU power and verified.');
 
-  await app.close();
-});
+//   await app.close();
+// });
